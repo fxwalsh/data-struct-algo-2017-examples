e4425ac6790b916057f22c08237d6d7bd09aa841
fxwalsh <fxwalsh@wit.ie>
2016-10-12 12:11:41 +0100

    update to eggs simulation

--- src_analysislab/ThrowEggsMain.java
+++ src_analysislab/ThrowEggsMain.java
@@ -1,4 +1,13 @@
 
+/**
+ * @author FXWALSH
+ * 
+ * Simulates throwing eggs problem from Algorithms analysis lab.
+ * Building modelled as the String array argument of the main method(args). Example as follows:
+ * args = {"o","o","o","b","b","b"} represents a 6 story building where eggs begin to break on 3th floor, 
+ * F=3. Ground floor is a[0].
+ *
+ */
 public class ThrowEggsMain {
 
 	public static void main(String[] args) {
