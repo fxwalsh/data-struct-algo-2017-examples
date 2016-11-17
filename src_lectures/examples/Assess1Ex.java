ca556a65ba572219d09345bb08c3f5cedd49254c
fxwalsh <fxwalsh@wit.ie>
2016-11-17 10:55:31 +0000

    Added Assign example

---
+++ src_lectures/examples/Assess1Ex.java
@@ -0,0 +1,31 @@
+package examples;
+
+import java.util.Arrays;
+
+public class Assess1Ex {
+
+	public static int mystery(int target, int[] a) {
+		int N = a.length;
+		int cnt = 0;
+		
+	    Arrays.sort(a);;
+		for (int i = 0; i < N-1; i++)
+			for (int j = i+1; j < N; j++){
+				int key = target-a[i]-a[j];
+				if (Arrays.binarySearch(a,j+1,N, key) >0)
+							cnt++;
+			}
+		return cnt;
+	}
+
+	public static void main(String[] args) {
+
+		int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		int target = 6;
+
+		System.out.println(mystery(target, a));
+
+	}
+
+}
